02b66b4006dee2ca4e8cc8eacbe01c919239905c
BarPupko <barpupco@gmail.com>
2023-10-24 02:06:41 +0300

    New Project

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from tkinter import *
+from PIL import ImageTK,Image
 
 root = Tk()
-# creating a label widget
-myLabel = Label(root, text="hello world")
+root.title("Break from PC")
+root.iconbitmap('C:\coffee.ico')
 
+
+button_quit = Button(root,text="Quit",command=root.quit)
+button_quit.pack()
+def myClick():
+    myLabel = Label(root,text="Look I clicked the button")
+    myLabel.pack()
+# creating a label widget
+# myLabel1 = Label(root, text="hello world").grid(row=0,column=1)
+# myLabel2 = Label(root, text="my name is bar").grid(row=1,column=1)
+
+myButton = Button(root,text="Click me",command=myClick,fg="red")
+myButton.pack()
 # shoving it onto the screen
-myLabel.pack()
+
 
 # will loop constantly
 root.mainloop()
