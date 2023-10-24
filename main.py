d90ee9d0ba64982e081b8bdd931182d4347bd144
BarPupko <barpupco@gmail.com>
2023-10-24 03:42:08 +0300

    Adding timer and picture , now I want to cut every photo from the picture and represent a timer for each picture , then the picture will shuffle automaticlly

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
-from tkinter import *
-from PIL import ImageTK,Image
-
-root = Tk()
-root.title("Break from PC")
-root.iconbitmap('C:\coffee.ico')
+import tkinter as tk
+from PIL import Image, ImageTk
+import time
+import subprocess
 
 
-button_quit = Button(root,text="Quit",command=root.quit)
-button_quit.pack()
-def myClick():
-    myLabel = Label(root,text="Look I clicked the button")
-    myLabel.pack()
-# creating a label widget
-# myLabel1 = Label(root, text="hello world").grid(row=0,column=1)
-# myLabel2 = Label(root, text="my name is bar").grid(row=1,column=1)
+def open_program():
+    root = tk.Tk()
+    root.title("Break from PC")
+    root.iconbitmap(r'C:\coffee.ico')
+    root.attributes('-fullscreen', True)
 
-myButton = Button(root,text="Click me",command=myClick,fg="red")
-myButton.pack()
-# shoving it onto the screen
+    my_img = ImageTk.PhotoImage(Image.open(r'C:\img21.jpg'))
+
+    my_label = tk.Label(image=my_img)
+    my_label.pack()
+
+    # Set the size of the window (width x height)
+    root.geometry("800x600")  # Change the values as needed
+
+    button_quit = tk.Button(root, text="Quit", command=root.quit)
+    button_quit.pack()
+
+    root.mainloop()
 
 
-# will loop constantly
-root.mainloop()
+# Initial run
+open_program()
+
+# Schedule to run the program every hour (3600 seconds)
+while True:
+    time.sleep(3600)  # Sleep for an hour
+    open_program()
