4f4689d489f6d7ab31ae9f5ba90136c89e40aebc
BarPupko <barpupco@gmail.com>
2024-01-07 22:27:30 +0200

    Creating the first version of the app

--- main.py
+++ main.py
@@ -1,35 +1,56 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 import time
-import subprocess
+import threading
 
+class SlideshowApp:
 
-# this software is to make you rest everytime in a while.
+    def __init__(self, image_files):
+        self.root = tk.Tk()
+        self.root.iconbitmap(r'C:\coffee.ico')
 
-def open_program():
-    root = tk.Tk()
-    root.title("Break from PC")
-    root.iconbitmap(r'C:\coffee.ico')
-    root.attributes('-fullscreen', True)
+        self.root.title("Slideshow")
 
-    my_img = ImageTk.PhotoImage(Image.open(r'C:\img21.jpg'))
+        self.image_files = image_files
+        self.slide_label = tk.Label(self.root)
+        self.slide_label.pack()
+        self.root.geometry("880x600"); #screen size
+        self.image_index = 0
+        self.slide_timer = 20  # seconds for each slide
+        self.total_timer = len(image_files) * self.slide_timer  # total time for slideshow
 
-    my_label = tk.Label(image=my_img)
-    my_label.pack()
+        self.show_slide()
+        self.update_timers()
 
-    # Set the size of the window (width x height)
-    root.geometry("800x600")  # Change the values as needed
+    def show_slide(self):
+        image = Image.open(self.image_files[self.image_index])
+        photo = ImageTk.PhotoImage(image)
+        self.slide_label.config(image=photo)
+        self.slide_label.image = photo  # keep a reference!
 
-    button_quit = tk.Button(root, text="Quit", command=root.quit)
-    button_quit.pack()
+        self.image_index = (self.image_index + 1) % len(self.image_files)
+        self.root.after(self.slide_timer * 1000, self.show_slide)
 
-    root.mainloop()
+    def update_timers(self):
+        # Here you can update your timers, currently, it just prints the countdown
+        print(f"Time remaining for this slide: {self.slide_timer} seconds")
+        print(f"Total slideshow time remaining: {self.total_timer} seconds")
+        self.total_timer -= 1
+        self.root.after(1000, self.update_timers)
 
+    def start(self):
+        self.root.mainloop()
 
-# Initial run
-open_program()
+def run_slideshow():
+    # List of image paths
+    images = ["images\\IMG1.PNG", "images\\IMG2.PNG", "images\\IMG3.PNG"]
+    app = SlideshowApp(images)
+    app.start()
 
-# Schedule to run the program every hour (3600 seconds)
+# Run the slideshow in a separate thread
+threading.Thread(target=run_slideshow).start()
+
+# Main loop that triggers the slideshow every hour
 while True:
-    time.sleep(3600)  # Sleep for an hour
-    open_program()
+    time.sleep(3600)  # Wait for 1 hour
+    run_slideshow()
